b4cf76bc480e158587fb839340532e4025982d3a
hooney <hooney@dunamu.com>
2023-09-15 14:33:45 +0900

    [STPL-5179] Add route info to Transaction

--- spring-cloud-gateway/src/main/java/com/newrelic/labs/FilteringWebHandler_Instrumentation.java
+++ spring-cloud-gateway/src/main/java/com/newrelic/labs/FilteringWebHandler_Instrumentation.java
@@ -5,6 +5,8 @@ import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -13,7 +15,7 @@ import java.util.regex.Pattern;
 
 @Weave(type = MatchType.ExactClass, originalName = "org.springframework.cloud.gateway.handler.FilteringWebHandler")
 public abstract class FilteringWebHandler_Instrumentation {
-
+    
     @Trace(dispatcher = true)
     public Mono<Void> handle(ServerWebExchange exchange) {
 
@@ -22,9 +24,14 @@ public abstract class FilteringWebHandler_Instrumentation {
             final Pattern idPattern = Pattern.compile("^(?=[^\\s]*?[0-9])[-{}().:_|0-9]+$");
             final Pattern codPattern = Pattern.compile("^(?=[^\\s]*?[0-9])(?=[^\\s]*?[a-zA-Z])(?!\\{id\\}).*$");
 
+            final String gatewayRouteId = "gateway_route_id";
+            final String gatewayRouteUri = "gateway_route_uri";
+
             String path = exchange.getRequest().getPath().value();
 
             String simplifiedPath = path;
+
+            Route route = exchange.getRequiredAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
 
             final String[] splitPath = path.split("/");
 
@@ -46,6 +53,8 @@ public abstract class FilteringWebHandler_Instrumentation {
             NewRelic.setTransactionName("Web", simplifiedPath);
             NewRelic.getAgent().getLogger().log(Level.FINER,
                     "spring-cloud-gateway Instrumentation: Setting web transaction name to " + simplifiedPath);
+            NewRelic.addCustomParameter(gatewayRouteId, route.getId());
+            NewRelic.addCustomParameter(gatewayRouteUri, route.getUri().toString());
         } catch (Exception e) {
             System.out.println("ERROR spring-cloud-gateway Instrumentation: " + e.getMessage());
         }
